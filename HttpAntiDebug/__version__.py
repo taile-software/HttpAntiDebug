5d7422f22a1df47479efe2d8a50b61a372d2cfca
taile-software <tlsoftware.taile@gmail.com>
2023-11-24 00:49:53 +0700

    update pkg

--- HttpAntiDebug/__version__.py
+++ HttpAntiDebug/__version__.py
@@ -1,6 +1,6 @@
 __title__ = "HttpAntiDebug"
 __description__ = "Python HTTP for Humans."
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __author__ = "TLSoftware"
 __author_email__ = "tlsoftware.taile@gmail.com"
 __license__ = "HttpAntiDebug For Tool - Vietnamese"
